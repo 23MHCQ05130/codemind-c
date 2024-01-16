e1daada9e7e14c619f1b854ee3e2e3b1d7a039d5
Niharika <147487594+23MHCQ05130@users.noreply.github.com>
2024-01-16 17:14:58 +0530

    #include<stdio.h> int main() {     int P1,P2,P3,P4,P5;     scanf("%d%d%d%d%d",&P1,&P2,&P3,&P4,&P5);     if(P1>=10&&P2>=10&&P3>=10&&P4>=10&&P5>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int P1,P2,P3,P4,P5;
+    scanf("%d%d%d%d%d",&P1,&P2,&P3,&P4,&P5);
+    if(P1>=10&&P2>=10&&P3>=10&&P4>=10&&P5>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
